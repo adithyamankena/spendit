3d97df1debcfe67f4ab063be53dac0692f7c4fdc
Adithya <adithyapandu8@gmail.com>
2026-08-12 18:34:36 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "spendit-v16";
+const CACHE = "spendit-v17";
 const CORE = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
